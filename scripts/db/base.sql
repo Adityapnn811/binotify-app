bab7344ae8b7c01b80525299ab668cc8dbb1916e
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-10-22 18:44:31 +0700

    fix: Fixed database schema

--- scripts/db/base.sql
+++ scripts/db/base.sql
@@ -13,18 +13,18 @@ CREATE TABLE IF NOT EXISTS Album (
     Total_duration int NOT NULL,
     Image_path varchar(256) NOT NULL,
     Tanggal_terbit date NOT NULL,
-    Genre varchar(64) NOT NULL
+    Genre varchar(64)
 );
 
 CREATE TABLE IF NOT EXISTS Song (
     song_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     Judul varchar(64) NOT NULL,
-    Penyanyi varchar(64) NOT NULL,
+    Penyanyi varchar(64),
     Tanggal_terbit date NOT NULL,
-    Genre varchar(64) NOT NULL,
+    Genre varchar(64),
     Duration int NOT NULL,
     Audio_path varchar(256) NOT NULL,
-    Image_path varchar(256) NOT NULL,
+    Image_path varchar(256),
     album_id int NOT NULL,
     FOREIGN KEY (album_id) REFERENCES Album (album_id)
 );
