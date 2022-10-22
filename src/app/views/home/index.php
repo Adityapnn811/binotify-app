0bdb66f71ea87e019a43be5e1582e482a5e39063
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-10-23 01:59:07 +0700

    syntax: Added some id and delete unused code

--- src/app/views/home/index.php
+++ src/app/views/home/index.php
@@ -1,21 +1,14 @@
-
 <h1> Welcome to Binotify </h1>
 <form method="post" action="/search">
 <input type="hidden" name="page" value="1"/>
-<input type="text" name="q" placeholder="Masukkan judul, tahun, penyanyi" autocomplete="off" required/>
+<input type="text" name="q" id="q" placeholder="Masukkan judul, tahun, penyanyi" autocomplete="off" required/>
 <label for="sort">A-Z:</label>
-<select name="sort" id="sort-select">
+<select name="sort" id="sort">
     <option value="Asc">Ascending</option>
     <option value="Desc">Descending</option>
 </select>
 <label for="genre">Genre:</label>
-<input type="text" name="genre" placeholder="pop, rock" autocomplete="off"/>
+<input type="text" name="genre" id="genre" placeholder="pop, rock" autocomplete="off"/>
 
 <button type="submit">Search</button>
 </form>
-
-
-<div id="searchResult"></div>
-
-<script>
-</script>
