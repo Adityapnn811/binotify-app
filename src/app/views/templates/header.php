069e1ff6fedf10c2b000f3250e8238c74111cdc0
adeliaaaa <monicadelia88@gmail.com>
2022-10-24 05:26:05 +0700

    feat: navbar

--- src/app/views/templates/header.php
+++ src/app/views/templates/header.php
@@ -1,3 +1,8 @@
+<?php session_start(); /* Starts the session */
+    $uname = "user";
+    $is_admin = "as_admin";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,3 +15,24 @@
         <link rel="stylesheet" href="/css/search.css" >
     </head>
     <body>
+        <nav class="navbar">
+            <a>Navbar</a>
+            <?php if ($is_admin) : ?>
+                <a> Tambah Lagu</a>
+                <a> Tambah Album</a>
+                <a> Daftar Album</a>
+                <a> Logout</a>
+            <?php else : ?>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search Lagu</button>
+                </form>
+                <a href="./logout.php">Daftar Album</a>
+                <a href="./dashboard.php">Logout</a>
+            <?php endif; ?>
+        </nav>
+
+
+    </body>
+
+</html>
