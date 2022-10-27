445f3ccc0300f7be0c0d3b1d3fa24444691a4df5
adeliaaaa <monicadelia88@gmail.com>
2022-10-27 22:39:17 +0700

    fix: logo and title problem

--- src/app/views/login/index.php
+++ src/app/views/login/index.php
@@ -1,19 +1,21 @@
 <div class="login-background">
     <div class="login-title-container">
-        <img src="img/logo.png" alt="BINOTIFY" class="login-logo">
+        <img src="img/logo.png" class="login-logo">
         <a class="login-title" href="/home" > BINOTIFY </a>
     </div>
-    <a class="login-line-divider"></a>
 
+    <a class="login-line-divider"></a>
 
     <div class="login-container">
         <div class="login-form-container">
+            <?php if (!empty($data)) : ?>
+                <p class="login-error-msg"><?=$data["error"]?></p>
+            <?php endif; ?>
 
             <form class="login-form" name="form1" method="post" action="/login/run">
                 <div class="login-input">
                     <label class="login-input-text" for="uname" required> Email address or username </label>
                     <input class="login-input-box" type="text" id="user_name" name="user_name" placeholder="Email address or username">
-
                 </div>
 
                 <div class="login-input" >
@@ -32,16 +34,7 @@
             <P class="login-register-text">Don't have an account?</P>
 
             <button class="login-register-button" onclick="window.location.href='/register'" > SIGN UP FOR SPOTIFY </button>                
-<!-- 
-            <p>New User 
-                <a class="login-title" href="/register">Register Here</a>
-            </p> -->
 
         </div>
-
     </div>
-
-        <p style="color:blue;">
-            <?= !empty($data) ? $data["error"] : "" ?> 
-        </p>
 </div>
