a3a1562c5c5ac7b9a2848401b8acd758e1c0c904
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-11-20 01:11:24 +0700

    fix: Edited config.php according to latest db config

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -1,6 +1,6 @@
 <?php
     // Database
-    getenv('MYSQL_DBUSER') ? define("MYSQL_DBUSER", getenv('MYSQL_DBUSER')) : define("MYSQL_DBUSER", "user");
+    getenv('MYSQL_DBUSER') ? define("MYSQL_DBUSER", getenv('MYSQL_DBUSER')) : define("MYSQL_DBUSER", "root");
     getenv('MYSQL_DBHOST') ? define("MYSQL_DBHOST", getenv('MYSQL_DBHOST')) : define("MYSQL_DBHOST", "localhost");
     getenv('MYSQL_DBPASS') ? define("MYSQL_DBPASS", getenv('MYSQL_DBPASS')) : define("MYSQL_DBPASS", "aditya962");
     getenv('MYSQL_DBNAME') ? define("MYSQL_DBNAME", getenv('MYSQL_DBNAME')) : define("MYSQL_DBNAME", "binotifydb");
